31303e6f5b3fd2f6277bdcb6b073ad7f8e5e1149
bjoernUCN <103989859+bjoernUCN@users.noreply.github.com>
2023-05-11 09:26:30 +0200

    Update MovieInfo.cs

--- SemesterProjekt3Web/Models/MovieInfo.cs
+++ SemesterProjekt3Web/Models/MovieInfo.cs
@@ -9,6 +9,8 @@
         public string Genre { get; set; }
         public string PgRating { get; set; }
         public DateTime PremiereDate { get; set; }
+        public bool CurrentlyShowing { get; set; }
+        public string MovieUrl { get; set; }
 
     }
 }
